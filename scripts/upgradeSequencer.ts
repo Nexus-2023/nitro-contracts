127f06436d7b534230f0967a617076643441d521
Shivansh Anand <shivansh.anand@Shivanshs-MacBook-Air.local>
2024-07-08 11:07:47 +0530

    testing changes

--- scripts/upgradeSequencer.ts
+++ scripts/upgradeSequencer.ts
@@ -9,9 +9,9 @@ import { Toolkit4844 } from "../test/contract/toolkit4844";
 
 
 async function main() {
-    const ExecutorContract = "0x8d8fc75DCFCD9586e4721F2452C6aFfaF71f1233";
+    const ExecutorContract = "0xF9D3F8d16f7eb8A096f7ec557a7413BFeDD350A3";
     const SequencerInboxProxy = "0x33D3A8042c4d8B8a28b51AB102d684211Bd85B80";
-    const ProxyAdmin = "0xf9d3f8d16f7eb8a096f7ec557a7413bfedd350a3";
+    const ProxyAdmin = "0xEE5Ba29bFE35f584C06a0210405ADe7114ddC749";
     const [admin] = await ethers.getSigners();
     const reader4844 = (await Toolkit4844.deployReader4844(admin)).address
 
@@ -29,12 +29,17 @@ async function main() {
     )
 
     const proxyAdmin = await ethers.getContractFactory("ProxyAdmin");
+    const proxyAdminFactory = proxyAdmin.attach(ProxyAdmin);
+    const tx = await proxyAdminFactory.getProxyImplementation(SequencerInboxProxy)
+    console.log("tx: ", tx)
 
     const upgrade = upgradeExecutorFactory.attach(ExecutorContract);
-    var tx = await upgrade.executeCall(ProxyAdmin, proxyAdmin.interface.encodeFunctionData("upgrade", [SequencerInboxProxy, sequencerInbox.address]));
 
+    const upgradeTx = await upgrade.executeCall(ProxyAdmin, proxyAdmin.interface.encodeFunctionData("upgrade", [SequencerInboxProxy, sequencerInbox.address]));
+    console.log("upgrade Tx:", upgradeTx)
 
-    console.log(tx.hash)
+    const implementationTx = await proxyAdminFactory.getProxyImplementation(SequencerInboxProxy)
+    console.log("implementation Address:", implementationTx)
 }
 
 main()
